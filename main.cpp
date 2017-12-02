2f403e7b301b84fe75b23cd26844e32e537d0252
Unknown <earokiadoss@my.uri.edu>
2017-12-02 13:44:06 -0500

    Five draft of milestone 1

--- main.cpp
+++ main.cpp
@@ -4,23 +4,11 @@
 #include<fstream>
 #include<ostream>
 #include<vector>
-#include"tg.h"
+#include "tg.h"
 using namespace std;
 
-
-int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
-
-    double oink = distance(1.0,5.0,10);
-    cout<<oink<<endl;
-
-    vector<string> bark = helper("meowwoofauuuoink");
-    cout<<bark<<endl;
-
-
-    for(int i = 0; i < 19683; i++){
-        cout<<freqS[i]<<endl;
-    }
+int main() {
+    Lang x("thisisasentence");
+    x.print();
     return 0;
 }
